7b262d4baad77dd5ce98431fc4ea15426fdc7d1a
ageier <andreas.geier@holidaycheck.com>
2018-02-26 09:09:24 +0100

    Show hotel name & description instead of Hello World

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-document.getElementById('root').innerHTML = 'Hello World!'
+var hotel = {
+    id: '1aa4c4ad-f9ea-3367-a163-8a3a6884d450',
+    name: 'Dana Beach Resort',
+    description: 'Das modern ausgestattete 5-Sterne-Hotel Dana Beach Resort liegt direkt am eigenen Sandstrand, erbaut in einem einzigartigen Stil, mit hohem Komfort, mit bestem Service und einem umfangreichen Sport- & Freizeitangebot.'
+};
+
+var html = '<div><h1>' + hotel.name + '</h1><p>' + hotel.description + '</p></div>';
+document.getElementById('root').innerHTML = html;
