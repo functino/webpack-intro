5da9237dd37e61589043c9c2c05b556996c03b39
ageier <andreas.geier@holidaycheck.com>
2018-02-26 09:18:23 +0100

    Introduce ellpisize to shorten description

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+var ellipsize = require('ellipsize');
+
 document.getElementById('root').innerHTML = '<button id="showButton">Show Hotel</button>';
 
 document.getElementById('showButton').addEventListener('click', function() {
-    var html = '<div><h1>' + hotel.name + '</h1><p>' + hotel.description + '</p></div>';
+    var html = '<div><h1>' + hotel.name + '</h1><p>' + ellipsize(hotel.description, 50) + '</p></div>';
     document.getElementById('root').innerHTML = html;
 });
