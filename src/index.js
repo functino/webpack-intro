1bd7d3897284ce2d5476b695bb43f3a8998a62b4
ageier <andreas.geier@holidaycheck.com>
2018-02-26 09:13:19 +0100

    Move hotel data to own file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-var hotel = {
-    id: '1aa4c4ad-f9ea-3367-a163-8a3a6884d450',
-    name: 'Dana Beach Resort',
-    description: 'Das modern ausgestattete 5-Sterne-Hotel Dana Beach Resort liegt direkt am eigenen Sandstrand, erbaut in einem einzigartigen Stil, mit hohem Komfort, mit bestem Service und einem umfangreichen Sport- & Freizeitangebot.'
-};
-
 document.getElementById('root').innerHTML = '<button id="showButton">Show Hotel</button>';
 
 document.getElementById('showButton').addEventListener('click', function() {
