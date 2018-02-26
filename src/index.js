8a6e6c34f2840f21c6c5021e33aab8eab750861c
ageier <andreas.geier@holidaycheck.com>
2018-02-26 09:33:18 +0100

    Use destructuring, arrow-functions, const instead of boring old JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-var ellipsize = require('ellipsize');
-var hotel = require('./hotel');
+import ellipsize from 'ellipsize';
+import hotel from './hotel';
 
 document.getElementById('root').innerHTML = '<button id="showButton">Show Hotel</button>';
 
-document.getElementById('showButton').addEventListener('click', function() {
-    var html = '<div><h1>' + hotel.name + '</h1><p>' + ellipsize(hotel.description, 50) + '</p></div>';
+document.getElementById('showButton').addEventListener('click', () => {
+    const { name, description } = hotel;
+    const html = `<div>
+        <h1>${name}</h1>
+        <p>${ellipsize(description, 50)}</p>
+    </div>`;
     document.getElementById('root').innerHTML = html;
 });
